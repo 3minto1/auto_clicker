e42ccf120e458de4654fa39742ba92c968bd55fc
Developer <dev@auto-clicker.local>
2026-06-11 19:42:10 +0800

    feat: hide console window when running GUI on Windows

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # main.py
-from gui import AutoClickerGUI
+import sys
+import ctypes
+
+def hide_console_window():
+    """Hide the console window on Windows."""
+    if sys.platform == 'win32':
+        try:
+            # Get the console window handle
+            console_window = ctypes.windll.kernel32.GetConsoleWindow()
+            if console_window:
+                # SW_HIDE = 0
+                ctypes.windll.user32.ShowWindow(console_window, 0)
+        except Exception:
+            # If we can't hide the console, just continue
+            pass
 
 def main():
+    hide_console_window()
     app = AutoClickerGUI()
     app.run()
 
