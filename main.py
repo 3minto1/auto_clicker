65f7fed00662cc9439c81d2530252d1c5ac7e322
Developer <dev@auto-clicker.local>
2026-06-11 19:58:23 +0800

    fix: remove broken hide_console, add vbs launcher for windowless start

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 # main.py
-import sys
-import ctypes
-
-def hide_console_window():
-    """Hide the console window on Windows."""
-    if sys.platform == 'win32':
-        try:
-            # Get the console window handle
-            console_window = ctypes.windll.kernel32.GetConsoleWindow()
-            if console_window:
-                # SW_HIDE = 0
-                ctypes.windll.user32.ShowWindow(console_window, 0)
-        except Exception:
-            # If we can't hide the console, just continue
-            pass
+from gui import AutoClickerGUI
 
 def main():
-    hide_console_window()
     app = AutoClickerGUI()
     app.run()
 
